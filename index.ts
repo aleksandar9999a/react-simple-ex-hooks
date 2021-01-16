422baf12d28d13660f7eada89ea5c1d7fb809825
Alexander Todorov <aleksandar9999a@gmail.com>
2021-01-16 15:20:00 +0200

    add basic file structure

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log('work')
+import { useMount } from './hooks/useMount';
+import { useUnmount } from './hooks/useUnmount';
+import { useWatch } from './hooks/useWatch';
+import { useUpdate } from './hooks/useUpdate';
+
+export default {
+  useMount,
+  useUnmount,
+  useWatch,
+  useUpdate
+};
